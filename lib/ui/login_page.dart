4bf37735479b185a2150f928f3640d808f994ba8
kengiiGMs <kaikiaoto22@gmail.com>
2025-03-25 17:01:12 -0300

    feat: Basic authentication system + Input capture and validation logic + Static home page

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:anshinpet/ui/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,7 +9,32 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   bool _visible = false;
+
+  void _login(context) {
+    setState(() {
+      String email = emailController.text;
+      String password = passwordController.text;
+
+      if (email == "email@123" && password == "123") {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else {
+        final snack = SnackBar(
+          content: Text("Erro - Email ou Senha Incorretos"),
+          duration: Duration(seconds: 2),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snack);
+      }
+    });
+  }
 
   void _visiblePassword() {
     setState(() {
@@ -21,46 +47,65 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 10.0),
-              child: Image.asset("images/logo_full.png", height: 200),
-            ),
-            TextField(
-              decoration: InputDecoration(label: Text('Digite o seu Email')),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            Stack(
-              alignment: Alignment.centerRight,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    label: Text('Digite a sua Senha'),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _visible,
-                ),
-                IconButton(
-                  onPressed: () {
-                    _visiblePassword();
-                  },
-                  icon: Icon(
-                    _visible ? Icons.visibility : Icons.visibility_off,
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 20.0),
-              child: FloatingActionButton(
-                onPressed: () {},
-                child: Text("Entrar"),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Image.asset("images/logo_full.png", height: 200),
               ),
-            ),
-          ],
+              TextFormField(
+                decoration: InputDecoration(label: Text('Digite o seu Email')),
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Insira o Email";
+                  }
+                },
+              ),
+              Stack(
+                alignment: Alignment.centerRight,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('Digite a sua Senha'),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    controller: passwordController,
+                    obscureText: _visible,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Insira a Senha";
+                      }
+                    },
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _visiblePassword();
+                    },
+                    icon: Icon(
+                      _visible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20.0),
+                child: FloatingActionButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      _login(context);
+                    }
+                  },
+                  child: Text("Entrar"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
